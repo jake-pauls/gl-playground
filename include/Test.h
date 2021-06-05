c8e71f354224fb3c91c889e1a4e8ab15da728bd8
jacob-pauls <jakepauls99@gmail.com>
2021-06-04 22:28:48 -0700

    Started configuring CMake build, added readme

---
+++ include/Test.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+class Test {
+public:
+    inline void test() {
+        std::cout << "Test!" << std::endl;
+    }
+};
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Test.h"
+
+int main() {
+    std::cout << "Hello, World" << std::endl;
+    Test().test();
+    return 0;
+}
