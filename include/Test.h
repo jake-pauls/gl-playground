a6d3912bc5adc2db9e2d83c89bb6943c416ac004
jacob-pauls <jakepauls99@gmail.com>
2021-06-05 00:01:22 -0700

    Basic OpenGL Hello Triangle with CMake build for glfw submodule

--- include/Test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-class Test {
-public:
-    inline void test() {
-        std::cout << "Test!" << std::endl;
-    }
-};
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
-#include <iostream>
-#include "Test.h"
+#include <GLFW/glfw3.h>
 
-int main() {
-    std::cout << "Hello, World" << std::endl;
-    Test().test();
+int main(void) {
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Legacy OpenGL Hello Triangle */
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
